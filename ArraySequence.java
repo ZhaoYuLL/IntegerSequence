5680e2b843d0f09796cb0f6e3c59e29315564f84
Zhao Yu Lin <>
2020-12-08 11:48:57 -0500

    shorter constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -6,13 +6,10 @@ public class ArraySequence implements IntegerSequence{
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int [] other){
     currentIndex = 0;
-    data = new int[other.length];
-    for(int i = 0; i < other.length; i ++){
-      data[i] = other[i];
-    }
+    data = other;
   }
   public void reset(){
-    
+    currentIndex = data[0];
   }
   public int length(){
     return 1;
