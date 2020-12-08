0b9aec809b7b146995828340e710a31a7ac304da
Zhao Yu Lin <>
2020-12-08 12:07:12 -0500

    tested the 2nd consturctor a bit, need furthur testing, right now it works for if start > end

--- Tester.java
+++ Tester.java
@@ -1,11 +1,12 @@
 public class Tester{
   public static void main(String[] args){
-    int[]nums = {1,3,5,0,-1,3,9};
-    IntegerSequence as = new ArraySequence(nums);
+    IntegerSequence r = new Range(20,10);
+IntegerSequence as = new ArraySequence(r);
 
-  System.out.println("ArraySequence(array):");
-    while(as.hasNext()){
-      System.out.print(as.next()+", ");
-    }
+System.out.println("ArraySequence(seq):");
+while(as.hasNext()){
+  System.out.print(as.next()+", ");
+}
+System.out.println();
   }
 }
